63f49e07532cdf9f9c9eb0996cc39c1f0abff2de
yanqiangmiffy <1185918903@qq.com>
2019-03-29 22:54:08 +0800

    Java入门第一季

--- MoocJava/src/seasonone/HelloWorld.java
+++ MoocJava/src/seasonone/HelloWorld.java
@@ -1,6 +1,12 @@
 package seasonone;
 
+/**
+ * @author quincyqiang
+ * @version 1.0
+ */
+
 public class HelloWorld {
+    // comment
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
