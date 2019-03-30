54be403be9931c3b3d55658f040a7301e2bc1cdd
yanqiangmiffy <1185918903@qq.com>
2019-03-31 02:58:39 +0800

    boot demo

--- miaosha/src/main/java/com/miaoshaproject/App.java
+++ miaosha/src/main/java/com/miaoshaproject/App.java
@@ -1,10 +1,23 @@
 package com.miaoshaproject;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Hello world!
  */
+
+@EnableAutoConfiguration //sb自动化配置
+@RestController
 public class App {
+    @RequestMapping("/")
+    public String Home(){
+        return "hello world";
+    }
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        SpringApplication.run(App.class,args);
     }
 }
